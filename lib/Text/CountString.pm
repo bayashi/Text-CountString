679ce5107dceb343f24f797e31c9465f4493ce8e
bayashi <bayashi@cpan.org>
2014-04-25 22:41:01 +0900

    initial commit

---
+++ lib/Text/CountString.pm
@@ -0,0 +1,57 @@
+package Text::CountString;
+use strict;
+use warnings;
+use Carp qw/croak/;
+
+our $VERSION = '0.01';
+
+sub new {
+    my $class = shift;
+    my $args  = shift || +{};
+
+    bless $args, $class;
+}
+
+1;
+
+__END__
+
+=head1 NAME
+
+Text::CountString - one line description
+
+
+=head1 SYNOPSIS
+
+    use Text::CountString;
+
+
+=head1 DESCRIPTION
+
+Text::CountString is
+
+
+=head1 REPOSITORY
+
+Text::CountString is hosted on github
+<http://github.com/bayashi/Text-CountString>
+
+Welcome your patches and issues :D
+
+
+=head1 AUTHOR
+
+Dai Okabayashi E<lt>bayashi@cpan.orgE<gt>
+
+
+=head1 SEE ALSO
+
+L<Other::Module>
+
+
+=head1 LICENSE
+
+This module is free software; you can redistribute it and/or
+modify it under the same terms as Perl itself. See L<perlartistic>.
+
+=cut
---
+++ t/01_basic.t
@@ -0,0 +1,11 @@
+use strict;
+use warnings;
+use Test::More;
+
+use Text::CountString;
+
+can_ok 'Text::CountString', qw/new/;
+
+# write more tests
+
+done_testing;
